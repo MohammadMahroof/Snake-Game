245eeeb31cd8d60589ad9f20804e1aa65af41b3b
Mohammad Mahroof Abdul Waheed <mohammadmahroof29@gmail.com>
2026-09-09 17:47:13 +0530

    feat: implement snake body movement and animation

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("Black")
 screen.title("Snake Game")
+screen.tracer(0)
+
 starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
+segments = []
+
 for position in starting_position:
-    new_snake = Turtle(shape="square")
-    new_snake.color("White")
-    new_snake.penup()
-    new_snake.goto(position)
+    new_segment = Turtle(shape="square")
+    new_segment.color("White")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
 
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
 
-
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+    segments[0].left(90)
 
 
 

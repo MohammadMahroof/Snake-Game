2fb01e169773d28ee1c4138884d552db4f697003
Mohammad Mahroof Abdul Waheed <mohammadmahroof29@gmail.com>
2026-09-12 22:37:10 +0530

    Create Scoreboard class and score display

---
+++ scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+ALIGHNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+class Scoreboard(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score:{self.score}", align=ALIGHNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+        
+    

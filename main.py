a0cc78c8a2c52498ffa7a0edaa70081cbe56b15b
Mohammad Mahroof Abdul Waheed <mohammadmahroof29@gmail.com>
2026-09-13 13:29:22 +0530

    feat: implement snake tail collision detection

--- main.py
+++ main.py
@@ -41,6 +41,16 @@ while is_game_on:
         scoreboard.game_over()
 
 
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+
+        elif snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
+
+
 
 
 
